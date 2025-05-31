50a2b381d6a7f2dead359947a9f44628f45fab0c
Void <richmondnkrumah04@gmail.com>
2025-05-31 18:49:30 +0000

    added logo util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,18 +9,20 @@ export async function getTickerFromCompanyName(name: string) {
 
 export async function getCompanyData(companyName: string) {
     const ticker = await getTickerFromCompanyName(companyName);
-    const data = await yahooFinance.quoteSummary(ticker!, {modules: [
-    'price',
-    'summaryDetail',
-    'financialData',
-    'defaultKeyStatistics',
-    'earnings',
-    'incomeStatementHistory',
-    'cashflowStatementHistory',
-    'assetProfile',
-    'summaryProfile',
-    'majorHoldersBreakdown'
-  ],});
+    const data = await yahooFinance.quoteSummary(ticker!, {
+        modules: [
+            'price',
+            'summaryDetail',
+            'financialData',
+            'defaultKeyStatistics',
+            'earnings',
+            'incomeStatementHistory',
+            'cashflowStatementHistory',
+            'assetProfile',
+            'summaryProfile',
+            'majorHoldersBreakdown'
+        ],
+    });
 
     console.log("kk from the other side")
     console.log(Object.keys(data))
@@ -28,4 +30,16 @@ export async function getCompanyData(companyName: string) {
         return null
     }
     return data
+}
+
+export const getCompanyLogo = async (companyName: string) => {
+    const response = await fetch(`https://api.logo.dev/search?q=${companyName}`, {
+        headers: {
+            "Authorization": `Bearer: sk_a4BuH4mFQU-Z0OPNe70CFw`
+        }
+    })
+
+    const result = await response.json()
+    return result[0].logo_url
+    
 }
