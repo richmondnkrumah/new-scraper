e5aa9833fe90ab87092b23dce683ae593d130629
Void <richmondnkrumah04@gmail.com>
2025-06-06 09:58:15 +0000

    fixed image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["img.logo.dev","placehold.jp"]
+    domains: ["img.logo.dev","placehold.jp","cdn.brandfetch.io"]
   }
 };
 
