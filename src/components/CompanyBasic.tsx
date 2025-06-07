599172f61e5b9d0b6219173c06132ad58616d896
Void <richmondnkrumah04@gmail.com>
2025-06-07 09:38:24 +0000

    fixed logo parser

--- src/components/CompanyBasic.tsx
+++ src/components/CompanyBasic.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import type { _company_data } from '@/app/page'
+import { parse } from 'tldts';
 import Image from 'next/image'
 type Props = {
     data: _company_data,
@@ -20,7 +21,7 @@ const CompanyBasic = ({ data, logo }: Props) => {
         <div className="border-gray-200 p-3 rounded-xl bg-gray-100">
             <div className='flex items-center justify-between'>
                 <h2 className="text-xl font-bold text-blue-800">{data?.price?.symbol}</h2>
-                <img className="w-20 h-20" src={`https://cdn.brandfetch.io/${summarizedData?.Website?.replace("https://www.","")}/w/400/h/400?c=1id_2I0zC2VtTcLOq9q`} alt="Company Logo" />
+                <img className="w-20 h-20" src={`https://cdn.brandfetch.io/${parse(summarizedData?.Website!).domain}/w/400/h/400?c=1id_2I0zC2VtTcLOq9q`} alt="Company Logo" />
             </div>
             {Object.entries(summarizedData).map(([key, value]) => (
                 <div key={key} className="flex gap-2">
